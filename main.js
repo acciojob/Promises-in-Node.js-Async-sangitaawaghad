da31010d3b3ef0fa85552c30b46adc8022004972
Sangita Avghad <sangita.awaghad@gmail.com>
2026-07-07 18:16:49 +0530

    Sangita: refactor error

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
-        reject(err);
+        reject(err.message);
         return;
       }
       resolve(data);
